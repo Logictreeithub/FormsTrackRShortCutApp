43f20f12a56d1f4f77a085155262eab215aa1122
reddysridharlogictree <reddysridhar.kandagaddala@logictreeit.in>
2024-05-10 12:56:00 +0530

    shortcut url code

---
+++ FormsTrackR1/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace FormsTrackR1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.chkRemember = new System.Windows.Forms.CheckBox();
+            this.btnOpenEye = new System.Windows.Forms.Button();
+            this.btnCloseEye = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserName.Location = new System.Drawing.Point(32, 67);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(118, 20);
+            this.lblUserName.TabIndex = 0;
+            this.lblUserName.Text = "Email Address";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.Location = new System.Drawing.Point(38, 123);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(83, 20);
+            this.lblPassword.TabIndex = 1;
+            this.lblPassword.Text = "Password";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Location = new System.Drawing.Point(241, 218);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(103, 40);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "Sign In";
+            this.btnLogin.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUserName.Location = new System.Drawing.Point(163, 59);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(316, 32);
+            this.txtUserName.TabIndex = 3;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(163, 115);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(276, 32);
+            this.txtPassword.TabIndex = 4;
+            this.txtPassword.TextChanged += new System.EventHandler(this.txtPassword_TextChanged);
+            // 
+            // chkRemember
+            // 
+            this.chkRemember.AutoSize = true;
+            this.chkRemember.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkRemember.Location = new System.Drawing.Point(163, 169);
+            this.chkRemember.Name = "chkRemember";
+            this.chkRemember.Size = new System.Drawing.Size(141, 24);
+            this.chkRemember.TabIndex = 5;
+            this.chkRemember.Text = "Remember Me";
+            this.chkRemember.UseVisualStyleBackColor = true;
+            // 
+            // btnOpenEye
+            // 
+            this.btnOpenEye.DialogResult = System.Windows.Forms.DialogResult.Yes;
+            this.btnOpenEye.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnOpenEye.Image = ((System.Drawing.Image)(resources.GetObject("btnOpenEye.Image")));
+            this.btnOpenEye.Location = new System.Drawing.Point(437, 114);
+            this.btnOpenEye.Name = "btnOpenEye";
+            this.btnOpenEye.Size = new System.Drawing.Size(42, 33);
+            this.btnOpenEye.TabIndex = 7;
+            this.btnOpenEye.UseVisualStyleBackColor = true;
+            this.btnOpenEye.Click += new System.EventHandler(this.btnOpenEye_Click);
+            // 
+            // btnCloseEye
+            // 
+            this.btnCloseEye.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnCloseEye.Image = ((System.Drawing.Image)(resources.GetObject("btnCloseEye.Image")));
+            this.btnCloseEye.Location = new System.Drawing.Point(437, 114);
+            this.btnCloseEye.Name = "btnCloseEye";
+            this.btnCloseEye.Size = new System.Drawing.Size(42, 33);
+            this.btnCloseEye.TabIndex = 8;
+            this.btnCloseEye.UseVisualStyleBackColor = true;
+            this.btnCloseEye.Click += new System.EventHandler(this.btnCloseEye_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(531, 300);
+            this.Controls.Add(this.btnCloseEye);
+            this.Controls.Add(this.btnOpenEye);
+            this.Controls.Add(this.chkRemember);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUserName);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(549, 347);
+            this.MinimumSize = new System.Drawing.Size(549, 347);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sign In to FormsTrackR";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.CheckBox chkRemember;
+        private System.Windows.Forms.Button btnOpenEye;
+        private System.Windows.Forms.Button btnCloseEye;
+    }
+}
+
---
+++ FormsTrackR1/Form1.cs
@@ -0,0 +1,412 @@
+﻿using Google.Protobuf.Compiler;
+using MySqlX.XDevAPI;
+using MySqlX.XDevAPI.Common;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrackBar;
+
+namespace FormsTrackR1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MaximizeBox = false;
+        }
+
+        StagingFormsTrackRDBEntities trackRDBEntities = new StagingFormsTrackRDBEntities();
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var userName = txtUserName.Text;
+            var passWord = txtPassword.Text;
+            string URI = "https://stagingmservices.formstrackr.com/api/Account/Login";
+            var result = await GetFileInformation(URI, userName, passWord);
+            if (result != null)
+            {
+                dynamic userDetails = JsonConvert.DeserializeObject(result);
+                //var user = trackRDBEntities.AspNetUsers.FirstOrDefault(i => i.UserName == userName);
+                string userId = EncryptDecrypt.DESEncrypt(userDetails.Result.UserId.ToString());
+
+                //string Password = EncryptDecrypt.DESDecrypt(userDetails.Result.Password);
+                if (userDetails.Result.Username.ToString() == userName/* && Password == passWord*/)
+                {
+                    if (chkRemember.Checked == true)
+                    {
+                        Properties.Settings.Default.UserName = userName;
+                        Properties.Settings.Default.Password = passWord;
+                        Properties.Settings.Default.UserId = userDetails.Result.UserId;
+                        Properties.Settings.Default.Save();
+                    }
+                    else
+                    {
+                        Properties.Settings.Default.UserName = userName;
+                        Properties.Settings.Default.Password = "";
+                        Properties.Settings.Default.UserId = userDetails.Result.UserId;
+                        Properties.Settings.Default.Save();
+                    }
+                    string url = "https://staging.formstrackr.com/Account/Impersination" + "?Id=" + userId;
+                    OpenUrlInDefaultBrowser(url);
+                    this.Close();
+                }
+                else
+                {
+                    txtUserName.Text = "";
+                    txtPassword.Text = "";
+                    chkRemember.Checked = false;
+                    MessageBox.Show("Invalid Login Attempt");
+                }
+
+            }
+            else
+            {
+
+                txtUserName.Text = "";
+                txtPassword.Text = "";
+                chkRemember.Checked = false;
+                MessageBox.Show("Invalid Login Attempt");
+            }
+
+
+
+        }
+
+        static void OpenUrlInDefaultBrowser(string url)
+        {
+            Process.Start(url);
+        }
+
+        public class EncryptDecrypt
+        {
+            const string DesKey = "AQWSEDRF";
+            const string Desiv = "HGFEDCBA";
+            public static string DESDecrypt(string stringToDecrypt)//Decrypt the content
+            {
+                stringToDecrypt = stringToDecrypt.Replace(" ", "+");
+                string sEncryptionKey = "01234567890123456789";
+                byte[] key = { };
+                byte[] iV = { 10, 20, 30, 40, 50, 60, 70, 80 };
+                byte[] inputByteArray = new byte[stringToDecrypt.Length];
+                try
+                {
+                    key = Encoding.UTF8.GetBytes(sEncryptionKey.Substring(0, 8));
+                    DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+                    inputByteArray = Convert.FromBase64String(stringToDecrypt);
+                    MemoryStream ms = new MemoryStream();
+                    CryptoStream cs = new CryptoStream(ms, des.CreateDecryptor(key, iV), CryptoStreamMode.Write);
+                    cs.Write(inputByteArray, 0, inputByteArray.Length);
+                    cs.FlushFinalBlock();
+                    Encoding encoding = Encoding.UTF8;
+                    return encoding.GetString(ms.ToArray());
+                }
+                catch (System.Exception /*ex*/)
+                {
+                    return (string.Empty);
+                }
+            }
+
+            public static string DESEncrypt(string stringToEncrypt)// Encrypt the content
+            {
+                string sEncryptionKey = "01234567890123456789";
+                byte[] key = { };
+                byte[] iV = { 10, 20, 30, 40, 50, 60, 70, 80 };
+                byte[] inputByteArray; //Convert.ToByte(stringToEncrypt.Length) 
+
+                try
+                {
+                    key = Encoding.UTF8.GetBytes(sEncryptionKey.Substring(0, 8));
+                    DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+                    inputByteArray = Encoding.UTF8.GetBytes(stringToEncrypt);
+                    MemoryStream ms = new MemoryStream();
+                    CryptoStream cs = new CryptoStream(ms, des.CreateEncryptor(key, iV), CryptoStreamMode.Write);
+                    cs.Write(inputByteArray, 0, inputByteArray.Length);
+                    cs.FlushFinalBlock();
+                    return Convert.ToBase64String(ms.ToArray());
+                }
+                catch
+                {
+                    return (string.Empty);
+                }
+            }
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+            txtPassword.PasswordChar = '*';
+            if (btnCloseEye.Text == "true")
+            {
+                btnCloseEye.Text = "";
+                btnCloseEye.BringToFront();
+                txtPassword.PasswordChar = '*';
+            }
+            if (btnOpenEye.Text == "true")
+            {
+                btnOpenEye.Text = "";
+                btnOpenEye.BringToFront();
+                txtPassword.PasswordChar = '\0';
+            }
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            txtUserName.Text = Properties.Settings.Default.UserName;
+            txtPassword.Text = Properties.Settings.Default.Password;
+            int userId = Properties.Settings.Default.UserId;
+            var trackresult =await GetTrackingDetailsById(userId);            
+            if (trackresult != null)
+            {
+                string value = "";
+                //dynamic trackDetails = JsonConvert.DeserializeObject(trackresult);
+                //dynamic jsonObject = JObject.Parse(trackresult);
+                
+                //var logoutDateTime = jsonObject["Result"]["LogoutDateTime"].ToString();
+
+                JObject j = JObject.Parse(trackresult);
+                JToken nestedToken = j["Result"];
+                if (nestedToken is JObject nestedObject)
+                {
+                     value = nestedObject["LogoutDateTime"].ToString();
+                    // Now you can safely access the nested value
+                }
+
+                if (value == null && value == "")
+                {
+                    OpenUrlByFormLoad(txtUserName.Text, txtPassword.Text,userId);
+                }
+                else
+                {
+                    if (txtUserName.Text != "" && txtPassword.Text != "")
+                    {
+                        chkRemember.Checked = true;
+                    }
+                    else
+                    {
+                        txtUserName.Text = "";
+                        txtPassword.Text = "";
+                        chkRemember.Checked = false;
+                    }
+
+                }
+            }
+            else
+            {
+                txtUserName.Text = "";
+                txtPassword.Text = "";
+                chkRemember.Checked = false;
+            }
+
+        }
+        private async void OpenUrlByFormLoad(string userName, string password,int Id)
+        {
+            var userResult = await GetUserDetailsById(Id);
+            if(userResult != null)
+            {
+                dynamic userData = JsonConvert.DeserializeObject(userResult);
+                //var user = trackRDBEntities.AspNetUsers.FirstOrDefault(i => i.UserName == userName);
+                string userId = EncryptDecrypt.DESEncrypt(userData.Result.UserId.ToString());
+                if (userData != null)
+                {
+                    if (password != "")
+                    {
+                        string a = userData.Result.UserPassword;
+                        string Password = EncryptDecrypt.DESDecrypt(a);
+                        if (userData.Result.UserName == userName && Password == password)
+                        {
+                            string url = "https://staging.formstrackr.com/Account/Impersination" + "?Id=" + userId;
+                            OpenUrlInDefaultBrowser(url);
+                            this.Close();
+                        }
+                    }
+                    else
+                    {
+                        if (userData.Result.UserName == userName)
+                        {
+                            string url = "https://staging.formstrackr.com/Account/Impersination" + "?Id=" + userId;
+                            OpenUrlInDefaultBrowser(url);
+                            this.Close();
+                        }
+                    }
+
+                }
+            }
+
+            //if (password != "" && password != null)
+            //{
+            //    string userId = EncryptDecrypt.DESEncrypt(Id.ToString());
+                
+            //        string url = "https://staging.formstrackr.com/Account/Impersination" + "?Id=" + userId;
+            //        OpenUrlInDefaultBrowser(url);
+            //        this.Close();
+                
+            //}
+            //else
+            //{
+            //    if (userName != null && userName != "")
+            //    {
+            //        string userId = EncryptDecrypt.DESEncrypt(Id.ToString());
+            //        string url = "https://staging.formstrackr.com/Account/Impersination" + "?Id=" + userId;
+            //        OpenUrlInDefaultBrowser(url);
+            //        this.Close();
+            //    }
+            //}
+
+        }
+
+        //private void ChkShowPassword_CheckedChanged(object sender, EventArgs e)
+        //{
+        //    if(ChkShowPassword.Checked == true)
+        //    {  
+        //        txtPassword.PasswordChar = '\0';               
+        //    }
+        //    else
+        //    {
+        //        txtPassword.PasswordChar = '*';
+        //    }
+        //}
+
+        private void btnOpenEye_Click(object sender, EventArgs e)
+        {
+            btnOpenEye.Text = "false";
+            btnCloseEye.Text = "true";
+
+            if (txtPassword.PasswordChar == '*')
+            {
+                btnOpenEye.BringToFront();
+                txtPassword.PasswordChar = '\0';
+            }
+            if (txtPassword.PasswordChar == '\0')
+            {
+                btnCloseEye.BringToFront();
+                txtPassword.PasswordChar = '*';
+            }
+
+        }
+
+        private void btnCloseEye_Click(object sender, EventArgs e)
+        {
+
+            btnCloseEye.Text = "false";
+            btnOpenEye.Text = "true";
+
+            if (txtPassword.PasswordChar == '\0')
+            {
+                btnCloseEye.BringToFront();
+                txtPassword.PasswordChar = '*';
+            }
+            if (txtPassword.PasswordChar == '*')
+            {
+                btnOpenEye.BringToFront();
+                txtPassword.PasswordChar = '\0';
+
+            }
+
+        }
+
+        public async Task<string> GetFileInformation(string url, string email, string password)
+        {
+            string responseData = null;
+            using (var client = new HttpClient())
+            {
+                var parameters = new Dictionary<string, string>
+                {
+                    { "DeviceType","iphone"},
+                    { "UniqueDeviceID","B07D817B-8267-4196-9C36-366593E9ECCC" },
+                    { "IpAddress","192.168.1.37" },
+                    { "DeviceInfo","Device Model - iPhone Device Name - iPhone SE (3rd generation) OS Name - iOS OS Version - 17.0.1 Unique Device ID - B07D817B-8267-4196-9C36-366593E9ECCC" },
+                    { "OSVersion","17.0.1"},
+                    { "Password",password },
+                    { "PushTokenID","808715C05FB098BA1D4CF9037DDF3AA6B9EC92980719B7E91C18E6EF2D485AA28D692B087F94D6ED9133DC2602DA3E013A8F56F1E417E12E64B480C4DDAD66C56F249A30AE97893063AD0EE88FB8E806" },
+                    {"Email",email}
+                };
+
+                dynamic json = JsonConvert.SerializeObject(parameters);
+
+                HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PostAsync(url, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    responseData = await response.Content.ReadAsStringAsync();
+                    //Data data = JsonConvert.DeserializeObject(responseData[1]);
+                    return responseData;
+                }
+
+            }
+            return responseData;
+        }
+
+        public async Task<string> GetTrackingDetailsById(int id)
+        {
+            string data = null;
+            using (HttpClient client = new HttpClient())
+            {
+                // Replace the URL with the actual API endpoint and parameters
+                string apiUrl = "https://stagingmservices.formstrackr.com/api/Account/GetUserLoginTrackingData?UserId=" + id;
+
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    data = await response.Content.ReadAsStringAsync();
+                    return data;
+                }
+                else
+                {
+                    Console.WriteLine($"Error: {response.StatusCode}");
+                }
+            }
+            return data;
+        }
+
+
+        public async Task<string> GetUserDetailsById(int id)
+        {
+            string data = null;
+            using (HttpClient client = new HttpClient())
+            {
+                // Replace the URL with the actual API endpoint and parameters
+                string apiUrl = "https://stagingmservices.formstrackr.com/api/Account/GetUserLoginData?UserId=" + id;
+
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    data = await response.Content.ReadAsStringAsync();
+                    return data;
+                }
+                else
+                {
+                    Console.WriteLine($"Error: {response.StatusCode}");
+                }
+            }
+            return data;
+        }
+
+        class Data
+        {
+            public string DeviceType { get; set; }
+            public string UniqueDeviceID { get; set; }
+            public string IpAddress { get; set; }
+            public string DeviceInfo { get; set; }
+            public string OSVersion { get; set; }
+            public string Password { get; set; }
+            public string PushTokenID { get; set; }
+            public string Email { get; set; }
+        }
+    }
+}
